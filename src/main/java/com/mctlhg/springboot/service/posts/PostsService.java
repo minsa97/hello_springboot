2afd2f1affc6ba39ad3c7f471822e5cb25eb9659
mct <minsa97@naver.com>
2022-03-14 17:44:00 +0900

    2022년 3월14일 - JPA 데이타베이스 - 등록까지 했음

---
+++ src/main/java/com/mctlhg/springboot/service/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.mctlhg.springboot.service.posts;
+
+import com.mctlhg.springboot.domain.posts.PostsRepository;
+import com.mctlhg.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
