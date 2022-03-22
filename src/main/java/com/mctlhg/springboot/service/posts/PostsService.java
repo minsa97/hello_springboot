7fde2b188b16f2d55aefc6bbf0fd390b1486ed38
mct <minsa97@naver.com>
2022-03-22 12:09:47 +0900

    2022년 3월22일 - mustache로 게시글 수정,삭제

--- src/main/java/com/mctlhg/springboot/service/posts/PostsService.java
+++ src/main/java/com/mctlhg/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.mctlhg.springboot.service.posts;
 
 import com.mctlhg.springboot.domain.posts.Posts;
 import com.mctlhg.springboot.domain.posts.PostsRepository;
+import com.mctlhg.springboot.web.dto.PostsListResponseDto;
 import com.mctlhg.springboot.web.dto.PostsResponseDto;
 import com.mctlhg.springboot.web.dto.PostsSaveRequestDto;
 import com.mctlhg.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,19 @@ public class PostsService {
                 () -> new IllegalArgumentException("해당 게시글이 없습니다."));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 }
