a87ef4ee31defddcfce038c2430c737fce4e1748
mct <minsa97@naver.com>
2022-03-23 11:29:28 +0900

    2022년 3월23일 - 구글 서비스 등록 후 로그인 테스트 완료

---
+++ src/main/java/com/mctlhg/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,18 @@
+package com.mctlhg.springboot.config.auth.dto;
+import com.mctlhg.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
